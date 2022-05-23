ffcb9081d003d772c2b328048d4c894ccddae8e8
Tristan Berezowski <42394588+tristanberezowski@users.noreply.github.com>
2022-05-23 15:14:35 -0700

    Updates resume data for titles and some links

--- src/resumeData.js
+++ src/resumeData.js
@@ -1,7 +1,7 @@
 let resumeData = {
     "imagebaseurl":"https://rbhatia46.github.io/",
-    "name": "Rahul Bhatia",
-    "role": "Frontend Developer and Data Scientist",
+    "name": "Tristan Berezowski",
+    "role": "Fullstack Web Developer",
     "linkedinId":"Your LinkedIn Id",
     "skypeid": "Your skypeid",
     "roleDescription": "I like dabbling in various parts of frontend development and like to learn about new technologies, write technical articles or simply play games in my free time.",
@@ -22,9 +22,9 @@ let resumeData = {
           "className":"fa fa-twitter"
         }
       ],
-    "aboutme":"I am currently a pre-final year student at The LNM Institute of Information Technology and pursuing my B.Tech from here. I am a self taught Full Stack Web Developer, currently diving deeper into Machine Learning. I believe that to be successful in life, one needs to be obsessive with their dreams and keep working towards them.",
-    "address":"India",
-    "website":"https://rbhatia46.github.io",
+    "aboutme":"I am an avid adventurer and problem solver. In my free time I enjoy playing and listening to music, playing games and sports with friends, and learning about new technologies. I dream of owning and apartment where I can host events and work from home.",
+    "address":"Canada",
+    "website":"bere.dev",
     "education":[
       {
         "UniversityName":"The LNM Insitute of Information Technology",
